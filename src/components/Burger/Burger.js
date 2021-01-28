9ff64a9ee66426e4120c247276f77fd01ec469a4
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-01-28 02:13:35 -0500

    reduce trans array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,23 @@ import classes from './Burger.css';
 
 const burger = (props) => {
     // covert object to an array of burgerIngredients
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(
         igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
         }
-    );
+    )
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
+    //console.log(transformedIngredients);
 
     return (
         <div className={classes.Burger}>
