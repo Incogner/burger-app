4dcaa6e5e47745995b99a3854988d2ec43428395
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-01-28 02:06:32 -0500

    transform obj to array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,13 +1,23 @@
 import React from 'react';
+
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+    // covert object to an array of burgerIngredients
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(
+        igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        }
+    );
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
