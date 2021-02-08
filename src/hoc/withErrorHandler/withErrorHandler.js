17642b2c0d4d3c40b9a2d3ec4189d9b6b8d1982f
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-02-07 21:10:23 -0500

    unmount interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,16 +7,22 @@ const withErrorHandler = (WrappedContent, axios) => {
     return class extends Component {
         constructor(props){
             super(props);
-            axios.interceptors.request.use(req => {
+            this.resInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             })
-            axios.interceptors.response.use(resp => resp, error => {
+            this.reqInterceptor = axios.interceptors.response.use(resp => resp, error => {
                 this.setState({error: error});
             })
         }
+
         state = { 
             error: null
+        }
+
+        componentWillUnmount = () => {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.request.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
