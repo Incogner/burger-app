83da4f750f15476e5d6b7f224dbd8fec4602a69f
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-04-21 23:35:25 -0400

    fetch orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,27 @@
 import React, {Component} from 'react';
+import axios from '../../axios-orders';
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+    componentDidMount () {
+        axios.get('/orders.json').then(
+            res=> {
+                const fetchedOrders = [];
+                for (let key in res.data){
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                console.log(res.data);
+                this.setState({loading: false, orders: fetchedOrders});
+            }
+        ).catch(err=> {
+            this.setState({loading: false});
+        });
+    }
+
     render () {
         return (
             <div>
@@ -12,4 +32,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
