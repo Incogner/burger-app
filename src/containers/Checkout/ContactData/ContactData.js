f5e033f7454362130787bf4246fd4e7995743920
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-04-23 01:06:18 -0400

    create inputs dynamically

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -93,14 +93,25 @@ class ContactData extends Component {
     }
 
     render () {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (
             <form>
-                    <Input elementType="..." elementConfig="..." value="..."/>
-                    <Input type="email" name="email" placeholder="Your Email"/>
-                    <Input type="text" name="street" placeholder="Street"/>
-                    <Input type="text" name="postal" placeholder="Postal Code"/>
-                    <Button btnType="Success"  clicked={this.orderHandler}>ORDER</Button>
-                </form>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value} />
+                ))}
+                
+                <Button btnType="Success"  clicked={this.orderHandler}>ORDER</Button>
+            </form>
         );
         if (this.state.loading) {
             form = <Spinner/>;
