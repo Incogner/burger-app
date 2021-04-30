05cd0976c9ffda8bca086ca6f00260ba4389b022
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2021-04-29 23:08:34 -0400

    fix routing errors

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -56,7 +56,7 @@ export const auth = (email, password, isSignup) => {
                 console.log(response);
                 const expirationDate =new Date(new Date().getTime() + response.data.expiresIn * 1000);
                 localStorage.setItem('token', response.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=[API_KEY]
+                localStorage.setItem('expirationDate', expirationDate);
                 localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
@@ -82,12 +82,12 @@ export const authCheckState = () => {
             dispatch(logout());
         } else {
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if(expirationDate > new Date()) {
+            if(expirationDate <= new Date()) {
+                dispatch(logout());
+            } else {
                 const userId = localStorage.getItem('userId');
                 dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
-            } else {
-                dispatch(logout());
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
             }
             
         }
